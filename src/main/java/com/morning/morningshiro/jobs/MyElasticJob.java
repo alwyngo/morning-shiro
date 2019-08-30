e8056f67810cad1eedfb4187e922bb463b9231f6
alwyngo <alwyn.xu@outlook.com>
2019-08-30 16:38:45 +0800

    添加elastic-job定时任务

--- src/main/java/com/morning/morningshiro/jobs/MyElasticJob.java
+++ src/main/java/com/morning/morningshiro/jobs/MyElasticJob.java
@@ -2,13 +2,22 @@ package com.morning.morningshiro.jobs;
 
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import com.morning.morningshiro.dao.entity.UserEntity;
+import com.morning.morningshiro.service.UserService;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 public class MyElasticJob implements SimpleJob {
+
+    @Resource
+    private UserService userService;
 
     @Override
     public void execute(ShardingContext context) {
         // 根据分片项执行任务
         System.out.println(context.toString());
+        List<UserEntity> list = userService.list();
         switch (context.getShardingItem()) {
             case 0:
                 break;
